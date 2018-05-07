2a960b5e5200fbc714bc2360536a5854d46378d4
Elias1103 <171wita29@o365.htl-leoben.at>
2018-05-07 16:22:39 +0200

    Schaltjahr

---
+++ src/Schaltjahr.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Schaltjahr {
+
+	public static void main(String[] args) {
+		// Jahr eingeben
+		String eingabe = JOptionPane.showInputDialog("Bitte geben sie das gewünsche Jahr ein!");
+		JOptionPane.showMessageDialog(null, eingabe);
+				
+		// Überüprüfen ob ein Schaltjahr
+		//mit Funktionen!
+		
+		// z.B 2016 ist ein Schaltjahr
+
+	}
+
+}
