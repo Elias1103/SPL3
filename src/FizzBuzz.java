a8ae724f77f33c65f31a6fa379112c9b1290f9c9
Elias1103 <171wita29@o365.htl-leoben.at>
2018-05-07 14:41:45 +0200

    FizzBuzz, Version 3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,20 +2,24 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		for (int zahl = 1; zahl <= 100; zahl++) {
-			FizzBuzz(zahl);
+		/*for (int zahl = 1; zahl <= 100; zahl++) {
+			FizzerBuzzer(zahl);
+		}*/
+		for(int zahl = 0; zahl <= 100; zahl+=4) {
+			FizzerBuzzer(zahl, false);
 		}
 	}
 
-	public static void FizzBuzz(int zahl) {
-		if (zahl % 3 == 0 && zahl % 5 == 0) {
-			System.out.println(zahl + " = FizzBuzz");
-		} else if (zahl % 5 == 0) {
-			System.out.println(zahl + " = Buzz");
-		} else if (zahl % 3 == 0) {
-			System.out.println(zahl + " = Fizz");
+	public static void FizzerBuzzer(int zahl, boolean showFizzBuzz) {
+		if (zahl % 4 == 0 && zahl % 7 == 0 && showFizzBuzz == true) {
+			System.out.println(zahl + " = FizzerBuzzer");
+		} else if (zahl % 4 == 0) {
+			System.out.println(zahl + " = Fizzer");
+		} else if (zahl % 7 == 0) {
+			System.out.println(zahl + " = Buzzer");
 		} else {
 			System.out.println(zahl);
 		}
 	}
 }
+
